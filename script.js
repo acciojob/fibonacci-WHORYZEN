ce84f43d8b0b3333bf9301c65ae6e82e8c4633df
WHORYZEN <kanishkshkl@gmail.com>
2024-06-29 21:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) return 0; // Base case: Fibonacci(0) is 0
+    if (num === 1) return 1; // Base case: Fibonacci(1) is 1
+    
+    let a = 0;
+    let b = 1;
+    let result = 0;
+    
+    for (let i = 2; i <= num; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    
+    return result;
 }
 
 module.exports = fibonacci;
